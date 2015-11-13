9ace5344aa6d0c37e3b3d8ffe5f1dd04c3d54804
Zuleima Rodriguez <bitland.tj@gmail.com>
2015-11-13 08:28:15 -0800

    Se agrego la tabla Clientes

---
+++ BaseDatosBITland/ProyectoFinal/BITland.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseDatosBITland.ProyectoFinal
+{
+    public class BITland : DbContext{
+        public DbSet<Cliente> Client { get; set; }
+    }
+
+    
+
+    }
+
---
+++ BaseDatosBITland/ProyectoFinal/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace BaseDatosBITland.ProyectoFinal
+{
+    public class Cliente
+    {
+        public int id { get; set; }
+        public string Tienda { get; set; }
+        public string Nombre { get; set; }
+        public string Nivel { get; set; }
+        public string Direccion { get; set; }
+    }
+}
